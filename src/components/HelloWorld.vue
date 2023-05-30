8b0b9a6254fea7676a597a6dfc78098184f0af89
chenbitao <cbtpro@163.com>
2023-05-30 22:28:55 +0800

    feat: 添加axios支持

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from '@/network'
+
 defineProps<{
   msg: string
 }>()
+
+let userInfo = ref<IUserInfo | null>(null)
+const getUserInfo = () => {
+  return new Promise<IUserInfo>((resolve, reject) => {
+    axios<IUserInfo>({
+      url: '/user/get_user',
+      method: 'GET'
+    })
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+onMounted(async () => {
+  try {
+    const user = await getUserInfo()
+    userInfo.value = user
+  } catch (error) {
+    console.error('获取用户失败，error：', error)
+  }
+})
 </script>
 
 <template>
@@ -12,6 +40,8 @@ defineProps<{
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    {{ userInfo }}
+    <a-button type="primary">Primary Button</a-button>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,21 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/gateway': {
+        target: 'http://127.0.0.1:7001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/gateway/, ''),
+        configure: (proxy, options) => {
+          // proxy 是 'http-proxy' 的实例
+        },
+      },
+      '/socket.io': {
+        target: 'ws://localhost:5174',
+        ws: true,
+      },
+    },
+  },
 })
